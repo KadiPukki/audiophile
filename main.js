f1cfab99b3bbf123d5f88371192bbba0a976ad61
Kadidja Pukki <kadidjatoure@hotmail.com>
2022-08-08 21:16:43 +0300

    products

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-/*function popupToggle() {
-    const popup = document.getElementById('popup');
-    popup.classList.toggle('active')
-}*/
-
 function popupToggle() {
     const popup = window.open('popup.html');
 }
+
